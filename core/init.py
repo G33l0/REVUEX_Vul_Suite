00c7352e791592117efe57fdbe7b8f59373d7132
G33l0 <121323412+G33l0@users.noreply.github.com>
2025-12-14 08:49:45 -0800

    Initialize core module with imports and metadata

---
+++ core/init.py
@@ -0,0 +1,10 @@
+"""
+REVUEX Vulnerability Suite - Core Modules
+Author: G33L0 | Telegram: @x0x0h33l0
+"""
+
+from .intelligence_hub import IntelligenceHub
+from .logger import RevuexLogger
+from .report_generator import ReportGenerator
+
+__all__ = ['IntelligenceHub', 'RevuexLogger', 'ReportGenerator']
