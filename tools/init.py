0ce80ba614829b10ca902c152d3b51935c493fbf
G33l0 <121323412+G33l0@users.noreply.github.com>
2025-12-15 08:47:01 -0600

    Update init.py

--- tools/init.py
+++ tools/init.py
@@ -1,8 +1,4 @@
-"""
-REVUEX Vulnerability Suite - Scanning Tools
-Author: G33L0 | Telegram: @x0x0h33l0
-"""
-
+# Original tools
 from .subdomain_hunter import SubdomainHunter
 from .tech_fingerprinter import TechFingerprinter
 from .js_secrets_miner import JSSecretsMiner
@@ -12,6 +8,19 @@ from .apk_analyzer import APKAnalyzer
 from .race_tester import RaceConditionTester
 from .price_scanner import PriceManipulationScanner
 
+# NEW advanced scanners
+from .ssrf_scanner import SSRFScanner
+from .sqli_scanner import EnhancedSQLiScanner
+from .idor_tester import IDORTester
+from .xss_scanner import EnhancedXSSScanner
+from .business_logic_abuser import BusinessLogicAbuser
+from .file_upload_tester import FileUploadTester
+from .xxe_scanner import XXEScanner
+from .session_analyzer import SessionAnalyzer
+from .cors_scanner import CORSScanner
+from .csrf_tester import CSRFTester
+from .dependency_checker import DependencyChecker
+
 __all__ = [
     'SubdomainHunter',
     'TechFingerprinter',
@@ -20,5 +29,16 @@ __all__ = [
     'JWTAnalyzer',
     'APKAnalyzer',
     'RaceConditionTester',
-    'PriceManipulationScanner'
+    'PriceManipulationScanner',
+    'SSRFScanner',
+    'EnhancedSQLiScanner',
+    'IDORTester',
+    'EnhancedXSSScanner',
+    'BusinessLogicAbuser',
+    'FileUploadTester',
+    'XXEScanner',
+    'SessionAnalyzer',
+    'CORSScanner',
+    'CSRFTester',
+    'DependencyChecker',
 ]
